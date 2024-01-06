78bfdaa845ad6de570bbcdd41076803a936218ba
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2024-01-06 09:46:37 +0530

    import java.util.Scanner; public class Frames{     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int p=(n*5)+(m*7);         System.out.println(p);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Frames{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int p=(n*5)+(m*7);
+        System.out.println(p);
+    }
+}
