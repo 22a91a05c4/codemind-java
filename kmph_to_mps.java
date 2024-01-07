c503e7591eabebe7f00a4c4a3ed692d4b6105e07
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2024-01-07 10:15:50 +0530

    import java.util.*; public class k_m{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int k=sc.nextInt();         double m=k*0.277778;         System.out.printf("%.2f",m);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class k_m{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        double m=k*0.277778;
+        System.out.printf("%.2f",m);
+    }
+}
