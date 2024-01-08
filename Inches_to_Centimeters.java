dd0f110b0a34ccaf952582b2c8a12af36130f488
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2024-01-08 10:11:11 +0530

    import java.util.*; public class inche{     public static void main(String[] args)     {         Scanner sc= new Scanner(System.in);         double i=sc.nextDouble();         double c=i*2.54;         System.out.printf("%.2f",c);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class inche{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        double i=sc.nextDouble();
+        double c=i*2.54;
+        System.out.printf("%.2f",c);
+    }
+}
