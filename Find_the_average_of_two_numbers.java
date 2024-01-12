805d16fc32840fb8d9375ddf90f07a17682dd686
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2024-01-12 10:38:05 +0530

    import java.util.*; public class Circle{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double b=sc.nextInt();         double a=sc.nextInt();         System.out.printf("%.4f",(a+b)/2);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Circle{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double b=sc.nextInt();
+        double a=sc.nextInt();
+        System.out.printf("%.4f",(a+b)/2);
+    }
+}
