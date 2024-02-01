c410e459d09253f8532d9ba2b1c32fcd7044084e
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2024-02-01 11:33:38 +0530

    import java.util.*; public class rj{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int t=(x*5)+(y*10);         int h=t/z;         System.out.println(h);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class rj{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int t=(x*5)+(y*10);
+        int h=t/z;
+        System.out.println(h);
+    }
+}
