fcc595245b5950efce61f7bda67542df61ce96fb
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2024-02-02 11:32:49 +0530

    import java.util.*; public class mul{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         float a=sc.nextFloat();         float b=sc.nextFloat();         float m=a*b;         System.out.printf("%.2f",m);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class mul{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        float m=a*b;
+        System.out.printf("%.2f",m);
+    }
+}
