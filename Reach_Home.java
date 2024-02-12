ed257bba89e3129da8005d9f963598f3f1ccb329
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2024-02-12 20:41:05 +0530

    import java.util.*; public class home{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         if((x*5)>=y)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Reach_Home.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class home{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if((x*5)>=y)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
